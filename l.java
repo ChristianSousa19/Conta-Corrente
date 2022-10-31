cc019ea06d1692fe0549aee5798004901a005767
ChristianSousa19 <christianssv37@gmail.com>
2022-10-31 19:49:13 -0300

    Initial commit

---
+++ l.java
@@ -0,0 +1,6 @@
+public class primeiro programa {
+public static void  main(String[]args) {
+    String.format("ola mundo.te amo christian", );
+}
+}
+
---
+++ src/Primeiroprograma.java
@@ -0,0 +1,5 @@
+public class Primeiroprograma {
+    public static void  main(String[]args) {
+        System.out.print("ola mundo,EU TE AMO CHRISTIAN");
+    }
+}
